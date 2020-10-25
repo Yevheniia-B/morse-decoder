f33c75d5e46c9418656ed4c68cb2c55237a68dd7
Yevheniia-B <evgenijar.work@gmail.com>
2020-10-25 12:00:34 +0200

    feat: add morse decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,63 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+decodeCharMap = {
+  '00': null,
+  '10': '.',
+  '11': '-',
+  
+};
+
+function splitChars(expr) {
+  let charCount = expr.length / 10;
+  let encodedChars = [];
+
+  for (let i = 0; i < charCount; i++) {
+
+    encodedChars.push(expr.slice(i * 10, (i * 10) + 10));
+  }
+  return encodedChars;
+}
+
+function convertMorseChar(elem) {
+  let morseChar = [];
+  if (elem === '**********') {
+     return ' ';
+  }
+
+  for (let i = 0; i < elem.length; i += 2) {
+    let item = elem.slice(i , i + 2);
+    let morseItem = decodeCharMap[item];
+     if (morseItem) {
+        morseChar.push(morseItem);
+     }
+  }
+  return morseChar.join('');
+
+}
+
+function convertToAlphabet(morseChars) {
+  let alphabetArray = [];
+
+  for (let i = 0; i < morseChars.length; i++) {
+    let alpabetChar = MORSE_TABLE[morseChars[i]];
+
+    if (alpabetChar) {
+      alphabetArray.push(alpabetChar);
+    }
+    else {
+      alphabetArray.push(morseChars[i]);
+    }
+  }
+  
+  return alphabetArray.join('');
+}
+
+
 function decode(expr) {
-    // write your solution here
+  let encodedChars = splitChars(expr);
+  let morseChars = encodedChars.map(convertMorseChar);
+  return convertToAlphabet(morseChars);
 }
 
 module.exports = {
